302a3db7196101a873479d1a94985fe6ebb91d74
unknown <hani1800016060@webmail.uad.ac.id>
2020-07-09 20:41:44 +0800

    Login CI

---
+++ app/Controllers/Hello.php
@@ -0,0 +1,13 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Hello extends Controller
+{
+	public function index()
+	{
+	$data['title'] = 'Helloworld'
+	$data['hello'] = 'Helloworld from CodeIgniter 4';
+	return view('hello_view',$data);
+	}
+}
---
+++ app/Controllers/Login.php
@@ -0,0 +1,37 @@
+<?php namespace App\Controllers;
+use App\Models\User_model;
+
+class Login extends BaseController
+{
+    public function index()
+    {
+        return view('user_form');
+    }
+
+    public function login_action()
+    {
+        $usermodel = new User_model();
+
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+
+        $cek = $usermodel->get_data($email, $password);
+
+        if (($cek['user_email'] == $email) && ($cek['user_pass'] == $password))
+        {
+            session()->set('user_email', $cek['user_email']);
+            session()->set('user_nama', $cek['user_nama']);
+            session()->set('user_id', $cek['user_id']);
+            return redirect()->to(base_url('user'));
+        } else{
+            session()->setFlashdata('gagal', 'username / password salah');
+            return redirect()->to(base_url('login'));
+        }
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to(base_url('login'));
+    }
+}
